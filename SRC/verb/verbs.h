fb977475ff265698dd538466b218844ef4d02cc0
Jason Linehan <patientulysses@gmail.com>
2012-03-25 10:33:50 -0400

    More escapees

---
+++ SRC/verb/verbs.h
@@ -0,0 +1,26 @@
+#ifndef __VERBS_H
+#define __VERBS_H
+
+
+struct verb_info {
+        int     id;
+        char    *name;
+        char    *desc;
+        wchar_t *icon;
+        short   pair;
+        void    (*say)(uint32_t from_key, uint32_t to_key);
+};
+
+
+#define MAXVERB 30
+struct verb_info verbs[MAXVERB];
+
+
+enum sayings { MSG_CANCEL, MSG_SMACK, SAY_INIT };
+bool say(int opt);
+
+
+#endif
+
+
+
