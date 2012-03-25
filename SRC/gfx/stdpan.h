d95ff321b063f59dc776f54e98928d7c1b81721b
Jason Linehan <patientulysses@gmail.com>
2012-03-25 10:37:13 -0400

    I need to be more scrupulous it seems

---
+++ SRC/gfx/stdpan.h
@@ -0,0 +1,11 @@
+#include "gfx.h"
+
+void stdpan(WINDOW *win, WINDOW **sub, WINDOW **buf, PANEL **pan);
+
+enum { DESC, MARK, POST };
+void menu_wins(MENU *menu, WINDOW *win, WINDOW *sub);
+void menu_look(MENU *menu, int option, bool setting, const char *ch);
+void menu_pair(MENU *menu, short fore, short back);
+void menu_control(MENU *menu, int esc);
+MENU *make_new_menu(char **name, char **description, void **usrptr, int n);
+
